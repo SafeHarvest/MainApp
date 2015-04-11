cac220695b64c3b5749e54c70388ddb4cb2d1d95
Jerry D'Antonio <stumpjumper@gmail.com>
2015-04-11 17:10:00 -0400

    Added browser as an ionic build target.

---
+++ IonicUpdated/platforms/browser/www/js/controllers/searchMap_Ctrl.js
@@ -0,0 +1,29 @@
+angular.module('searchMap.controllers',[])
+.controller('SearchMapCtrl',[ "$scope",'$http','dataService', function($scope,$http,dataService)  {
+		
+	$scope.values = [{
+	  id: 1,
+	  label: 'aLabel',
+	  subItem: { name: 'aSubItem' }
+	}, {
+	  id: 2,
+	  label: 'bLabel',
+	  subItem: { name: 'bSubItem' }
+	}];
+
+	$scope.selected = { name: 'aSubItem' };
+	
+  	$scope.changeLat = function(lat){
+		alert(lat);
+  	}
+  	$scope.changeLong = function(Long){
+		alert(Long);
+  	}
+  	$scope.changeAddress = function(address){
+		alert(address);
+  	}
+	
+  	$scope.geoLocate = function(){
+		alert("geoLocate");
+  	}
+}]);
