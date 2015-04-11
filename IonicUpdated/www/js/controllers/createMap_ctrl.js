7e15f0d37aef880d9ed8aa3376368babb0852a8a
TashaMC <Home@tmarkovichs-mbp.grc.nasa.gov>
2015-04-11 13:16:21 -0400

    Added fields for lat,long address

--- IonicUpdated/www/js/controllers/createMap_ctrl.js
+++ IonicUpdated/www/js/controllers/createMap_ctrl.js
@@ -28,32 +28,14 @@ angular.module('createmap.controllers', [])
         }
     });
 	
-  	// Simple POST request example (passing data) :
-  	/*$http.post('https://rocky-badlands-6969.herokuapp.com/').
-    success(function(data, status, headers, config) {
-	 		$scope.test = data;
-      // this callback will be called asynchronously
-      // when the response is available
-    }).
-    error(function(data, status, headers, config) {
-      // called asynchronously if an error occurs
-      // or server returns response with an error status.
-    });*/
-	/*$http.get('http://echo.jsontest.com/conditions/frightful').then(function(resp) {
-	    $scope.conditions = resp.data.conditions;
-	  }, function(err) {
-	    console.error('ERR', err);
-	    // err.status will contain the status code
-	  })*/
-		
-	var promise = dataService.getData();
-	promise.then(function(result) {
-		  //alert('Success: ' + result);
-		  $scope.conditions2 = result;
-	}, function(reason) {
-		  //alert('Failed: ' + reason);
-	}, function(update) {
-		  //alert('Got notification: ' + update);
-	});
-	
+  	$scope.changeLat = function(lat){
+		alert(lat);
+  	}
+  	$scope.changeLong = function(Long){
+		alert(Long);
+  	}
+  	$scope.changeAddress = function(address){
+		alert(address);
+  	}
+  	
 }])
