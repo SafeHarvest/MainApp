edef895bf3011762a66e241a5b6321316ecbcf33
Jerry D'Antonio <stumpjumper@gmail.com>
2015-04-12 14:54:21 -0400

    Built for platforms

--- IonicUpdated/platforms/browser/www/js/services/dataService.js
+++ IonicUpdated/platforms/browser/www/js/services/dataService.js
@@ -25,10 +25,23 @@ angular.module('data.service', [])
 
 		return deferred.promise;
 	}
-
-	var _getMapData = function(maxLat, minLat, maxLng, minLng, cropTypeId, pestTypeId){
+	
+	var _postCropData = function(){
 		var deferred = $q.defer();
-		$http.get('https://rocky-badlands-6969.herokuapp.com/mapData',
+		
+		/* $http.get('https://rocky-badlands-6969.herokuapp.com/crops').then(function(data,status,headers,config) {
+			deferred.resolve(data);
+		}, function(err) {
+	  	    //console.error('ERR', err);
+	  	    // err.status will contain the status code
+	  	}) */
+
+		return deferred.promise;
+	}
+
+	/*var _getMapData = function(maxLat, minLat, maxLng, minLng, cropTypeId, pestTypeId){
+		var deferred = $q.defer();
+		$http.get('https://rocky-badlands-6969.herokuapp.com/incidents',
 		{
 			maxLat: maxLat,
 			minLat: minLat,
@@ -39,7 +52,18 @@ angular.module('data.service', [])
 		}).then(function(data, status, headers, config){
 			deferred.resolve(data);
 		}, function(err){
-			alert("Unable to retrieve map data.");
+			//alert("Unable to retrieve map data.");
+		})
+
+		return deferred.promise;
+	}*/
+	
+	var _getMapData = function(){
+		var deferred = $q.defer();
+		$http.get('https://rocky-badlands-6969.herokuapp.com/incidents').then(function(data, status, headers, config){
+			deferred.resolve(data);
+		}, function(err){
+			//alert("Unable to retrieve map data.");
 		})
 
 		return deferred.promise;
